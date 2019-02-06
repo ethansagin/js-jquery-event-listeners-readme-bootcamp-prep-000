ee81e866623886c34f1be74dfc782e30e700e976
Ethan <ethansagin@gmail.com>
2019-02-06 21:49:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,9 +6,8 @@ function getIt() {
 
 function frameIt() {
   $('img').on('load', function() {
-    $('img').className = $('.tasty');
+    $('img.tasty');
   });
-  return $('img')
 }
 
 function pressIt() {
